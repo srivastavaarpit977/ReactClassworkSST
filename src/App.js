fd5126de5fb6b2c4ffae5e51cb370c5d3c4a8093
Arpit Srivastava <srivastavaarpit1909@gmail.com>
2024-06-24 10:43:05 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
-import Products from './components/Product/Products';
-import {a, b} from './components/Product/Products';
-
-
+import { a, b } from './components/Products/Products';
+import Products from './components/Products/Products';
+import { useState } from 'react';
+import CartContext from './context/CartContext';
 function App() {
+  // state variable
+  // inc
+  // dec
+  let [cart, setCart] = useState({});
+  function increaseQuantity(product) {
+    const newCart = { ...cart };
+    // if(cart[product.id])
+    if (!newCart[product.id]) {
+      newCart[product.id] = {
+        ...product,
+        quantity: 0
+      };
+    }
+    newCart[product.id].quantity += 1;
+    console.log(newCart);
+    setCart(newCart);
+  }
+
+  function decreaseQuantity(product) {
+    const newCart = { ...cart };
+    if (!newCart[product.id]) return;
+    newCart[product.id].quantity -= 1;
+    if (newCart[product.id].quantity <= 0) {
+      delete newCart[product.id];
+    }
+    setCart(newCart);
+  }
+
   console.log(a, b);
   return (
-    <div className="App">
-
-      
-      
-        
-
-      
-      <Products />
-      
-    </div>
+    <CartContext.Provider value={{ cart, increaseQuantity, decreaseQuantity}}>
+      <div className="App">
+        <Products  />
+      </div>
+    </CartContext.Provider>
   );
 }
 
-
 export default App;
+
+// {1:{id: 1, quantity: 11}, 2:{id: 2, quantity: 10}, 3:{id: 3, quantity: 10}, 4:{id: 4, quantity: 10}}
+
+// button
+// - 1 +
+
+// let a = {b:10, c:20};
+//a.b = 30;
+// let b = {...a};
+
+// a => 1234
+// a => 12
+
+// {cart: cart, increaseQuantity: increaseQuantity}
+
+
+// Global Parent
+// Global state
+// way to manipulate the state
+// way to consume the state
+
+
+//  Provider
+//  Store
+    // State
+    // Reducer
+// Action
+// Dispatcher
+// Selectors
--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,61 +1,73 @@
-import {useRef, useState } from 'react';
+import './ProductCard.css';
+import AddToCart from '../AddToCart/AddToCart';
+import { useRef, useState } from 'react';
+import logo1 from '../../assests/logo1.png';
+function ProductCard({ product }) {
+  var a = 10;
+  a = a + 1;
+  let pRef = useRef(0);
+  let iRef = useRef(0);
+  let oRef = useRef(0);
+  let [inputV, setInputV] = useState('Class');
+  console.log("pc", product.id)
 
-function ProductCard({title, price, k }) {
-    let spanRef = useRef(null); // Initialize ref for span
-    let pRef = useRef(null); // Initialize ref for p
-    let iRef = useRef(0); // Initialize ref for input
-    const [inputValue, setInputValue] = useState(''); // State to hold input value
-    const [displayText, setDisplayText] = useState(''); // State to hold the text to be displayed
-
-
-
-    function printTitle() {
-        console.log(title);
-        if (spanRef.current) {
-            spanRef.current.style.color = "red";
-            if (spanRef.current.style.display === 'none') {
-                spanRef.current.style.display = 'block';
-            } else {
-                spanRef.current.style.display = 'none';
-            }
-        }
-        if (pRef.current) {
-            if (pRef.current.style.display === 'none') {
-                pRef.current.style.display = 'block';
-            } else {
-                pRef.current.style.display = 'none';
-            }
-        }
+  // setInput(value);
+  function printTitle() {
+    // console.log("printTitle");
+    // console.log(pRef.current.innerText);
+    if (pRef.current.style.display === "none") {
+      pRef.current.style.display = "block";
+    } else {
+      pRef.current.style.display = "none";
     }
+  }
+  // console.log("Rerendered", inputV);
+  function displayOutput(e) {
+    // console.log(iRef.current);
+    // console.log(oRef.current);
+    // oRef.current.innerText = `Output here: ${ iRef.current.value }`;
+    // oRef.current.innerText = oRef.current.innerText + iRef.current.value;
+    setInputV(e.target.value);
+  }
 
-    function showInput(e) {
-        setInputValue(e.target.value);
-    }
-    function appendInput(e) {
-        if (e.key === 'Enter') {
-            setDisplayText(inputValue);
-            setInputValue('');
-        }
-        
-    }
-    
-    return (
-        <div>
-            <h3>{k} . <span onClick={printTitle}>{title}</span> = <span ref={spanRef} style={{ display: 'none' }}>{price.value}</span></h3>
-            <input 
-                type="text" 
-                placeholder="Enter Text" 
-                value={inputValue}
-                onChange={showInput}
-                onKeyPress={appendInput} 
-            />
-            
-            <div ref={iRef}  className='append'> {inputValue}</div>
-            <p ref={pRef} style={{ display: 'none' }}>This is a product card</p> 
-        </div>
-    );
-
-
-}
-
+  return (
+    <div className="product-card">
+      <p onClick={printTitle}> {product.title}</p>
+      <p ref={pRef}> {product.price.value}</p>
+      <img src={logo1} />
+      <input type="text" onChange={displayOutput} ref={iRef} value={inputV} />
+      <p ref={oRef}>Over here the output would arrive - {inputV}</p>
+      <AddToCart product={product} />
+    </div>
+    )
+  }
+  
 export default ProductCard;
+  
+
+//ProductCard()
+
+// useState
+// useRef
+// useEffect
+
+// custom hooks 
+// default hooks
+// they should not render a ui 
+// they should be on the top of a function 
+// they should be not in any conditon or loops 
+
+
+
+// state variable = 
+// whenver a state variable is set it would cause a rerender of the component
+// and the value of the variable would be retained on rerenders
+
+// function or component is called for the first time 
+// Mounting
+
+// useState(default value)
+// returns [stateVar, setterFn];
+
+
+// useEffect(fn, [])
