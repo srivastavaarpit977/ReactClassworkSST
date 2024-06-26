6f699d4ad3a1d7ea447b7e016a27b8e20dd16605
Arpit Srivastava <srivastavaarpit1909@gmail.com>
2024-06-26 11:26:16 +0530

    update

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,21 @@
+import {App} from './App';
+import { useSelector } from "react-redux";
+import './Cart.css';
+
+function Cart() {
+    const items = useSelector((state) => state.items);
+
+    return (
+        <div className='cart'>
+            <h2>Cart</h2>
+            {Object.keys(items).map((id) => (
+                <div key={id}>
+                    <span>Product ID: {id}</span>
+                    <span> &nbsp; Quantity: {items[id].quantity}</span>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Cart;
